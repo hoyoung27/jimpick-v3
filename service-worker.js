ba61457eaff69148fd0adcd970bf72bfc746a50c
hoyoung27 <hinaka27@naver.com>
2026-07-13 22:54:26 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='jimpick-pro-2-4-full-auto';
+const CACHE_NAME='jimpick-pro-2-5-smart-conditions';
 const FILES=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
