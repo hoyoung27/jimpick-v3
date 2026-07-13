77d9504ffd349f1f84f8b9b26010fae40a97f953
hoyoung27 <hinaka27@naver.com>
2026-07-13 20:13:44 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,22 @@
-const CACHE_NAME = 'jimpick-commercial-1-1-mvp';
-
-const APP_FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
-  );
+const CACHE_NAME='jimpick-pro-1-0';
+const FILES=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+  )));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request).then(cached =>
-          cached || caches.match('./index.html')
-        )
-      )
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));
+      return r;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
   );
 });
